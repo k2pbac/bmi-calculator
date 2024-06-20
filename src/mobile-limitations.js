b175c6585559f5771ef11ca871074adebc428351
k2pbac <krisbachan123@hotmail.com>
2024-06-20 17:05:38 -0400

    table and some mobile designs

--- src/mobile-limitations.js
+++ src/mobile-limitations.js
@@ -17,45 +17,50 @@ export default function MobileLimitations() {
       </div>
 
       <div className="section-2">
-        <Card
-          link="./icon-gender.svg"
-          alt="gender-icon"
-          title="Gender"
-          content="The development and body fat composition of girls and boys vary
+        <div className="cards">
+          <Card
+            link="./icon-gender.svg"
+            alt="gender-icon"
+            title="Gender"
+            content="The development and body fat composition of girls and boys vary
               with age. Consequently, a child's age and gender are considered
               when evaluating their BMI."
-        />
-        <img
-          className="curved-line"
-          src="./pattern-curved-line-right.svg"
-          alt="curved-line"
-        ></img>
-        <Card
-          link="./icon-age.svg"
-          alt="age-icon"
-          title="Age"
-          content="In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content."
-        />
-        <Card
-          link="./icon-muscle.svg"
-          alt="muscle-icon"
-          title="Muscle"
-          content="BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."
-        />
-        <Card
-          link="./icon-pregnancy.svg"
-          alt="pregnancy-icon"
-          title="Pregnancy"
-          content="Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."
-        />
-        <Card
-          link="./icon-race.svg"
-          alt="race-icon"
-          title="Race"
-          content="Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse."
-        />
+          />
+          <img
+            className="curved-line"
+            src="./pattern-curved-line-right.svg"
+            alt="curved-line"
+          ></img>
+          <Card
+            link="./icon-age.svg"
+            alt="age-icon"
+            title="Age"
+            content="In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content."
+          />
+        </div>
+        <div className="cards">
+          <Card
+            link="./icon-muscle.svg"
+            alt="muscle-icon"
+            title="Muscle"
+            content="BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."
+          />
+          <Card
+            link="./icon-pregnancy.svg"
+            alt="pregnancy-icon"
+            title="Pregnancy"
+            content="Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."
+          />
+        </div>
+        <div className="cards justify-center">
+          <Card
+            link="./icon-race.svg"
+            alt="race-icon"
+            title="Race"
+            content="Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse."
+          />
+        </div>
       </div>
-      {/* <div className="section-3"></div> */}
     </div>
   );
 }
